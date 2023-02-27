6509a55c752837e482357192a17ffcd1525ff74c
Sajjad <vosoughsajjad@gmail.com>
2023-02-27 14:04:27 +0330

    add read image function

--- src/function.py
+++ src/function.py
@@ -1,5 +1,5 @@
 import os
-
+from matplotlib import image as mt
 def read_txt(path):
     arr = []
     for folder in os.listdir(path):
@@ -14,6 +14,15 @@ def read_txt(path):
             i = y.split('\t')
             file_names.append(i[0])
             bounding_box.append([i[1], i[2], i[3], i[4].rstrip('\n')])
+            return file_names, bounding_box
+
+def red_image(image_list, path):
+    images = []
+    for image in image_list:
+        IMAGE_PATH = os.path.join(path, image.split('_')[0], 'images', image)
+        images.append(mt.imread(IMAGE_PATH))
+        return images
+
 
 
 
